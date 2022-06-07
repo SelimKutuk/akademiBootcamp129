af07d5fc7ead44d92697c07b9a0ab876498078f1
Zeynep-Su <zeysuk@gmail.com>
2022-06-07 23:12:13 +0300

    Düzeltme yapıldı

--- lib/Profile/profile.dart
+++ lib/Profile/profile.dart
@@ -38,120 +38,123 @@ class _Profile_ScreenState extends State<Profile_Screen> {
             colorMainBackGround, //Ekranin orta kısmının rengini buradan ayarlayabiliriz.
 
         body: Center(
-          child: Container(
-            child: Column(children: [
-              SizedBox(height: 50),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  IconButton(
-                    icon: Icon(
-                      Icons.keyboard_backspace,
-                      size: 30,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {
-                      Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(
-                          builder: (BuildContext context) => HomePage(
-                              etkinlikAdlariList: FirebaseExample
-                                  .PostEtkinlikAdlari, //Ana sayfaya geri dönme
-                              etkinlikProfilResim:
-                                  FirebaseExample.PostProfileImgs,
-                              etkinlikProfilIsimList:
-                                  FirebaseExample.PostIsimler,
-                              etkinlikResimList: FirebaseExample.PostImgs,
-                              etkinlikTarihList: FirebaseExample.PostTarihler,
-                              etkinlikKonumList: FirebaseExample.PostKonum),
-                        ),
-                        (route) => false,
-                      );
-                    },
-                  ),
-                ],
-              ),
-              Align(
-                alignment: Alignment.topRight,
-                child: IconButton(
-                  onPressed: (() {
-                    Navigator.pushAndRemoveUntil(
-                      context,
-                      MaterialPageRoute(
-                        // Profil >>> Profil Editleme
-                        builder: (BuildContext context) => Profile_Edit(),
-                      ),
-                      (route) => false,
-                    );
-                    ; //Burası profil bilgileri değiştirme kısmı
-                  }),
-                  icon: Icon(Icons.edit, size: 25, color: Colors.blue),
-                ),
-              ),
-              Photo_Edit(user: user), //!API!
-
-              SizedBox(height: 15),
-              Text(
-                "${user.name}",
-                style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
-              ), // İsim
-
-              //Diğer Bilgiler
-              Text("${user.location}"),
-              Column(children: [
-                Text(
-                  "Meslek: ${user.job}",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                Text("Yaş: ${user.age}",
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-              ]),
-
-              Column(
-                children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height / 2,
-                    width: MediaQuery.of(context).size.width,
-                    child: ListView.separated(
-                        shrinkWrap: true,
-                        scrollDirection: Axis.vertical,
-                        itemCount: user.Postnumber,
-                        separatorBuilder: (context, _) => SizedBox(height: 5),
-                        itemBuilder: (BuildContext context, int index) {
-                          return Container(
-                            child: Column(
-                              children: [
-                                Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        "Etkinlik Saati:20.00",
-                                      ),
-                                      Text("Etkinlik Yeri:İstanbul"),
-                                      Text("Tarih: 21.06.2022")
-                                    ]),
-                                SizedBox(
-                                    child: Image.network(
-                                  'https://previews.123rf.com/images/captainvector/captainvector1703/captainvector170301312/73848230-logo-universit%C3%A9.jpg',
-                                  height: 330,
-                                )),
-                                Divider(
-                                  thickness: 5,
-                                  indent: 20,
-                                  endIndent: 20,
-                                  color: Colors.black,
-                                )
-                              ],
+          child: ListView(
+                    children:[
+                      Column(children: [
+                        SizedBox(height: 50),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            IconButton(
+                              icon: Icon(
+                                Icons.keyboard_backspace,
+                                size: 30,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {
+                                Navigator.pushAndRemoveUntil(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (BuildContext context) => HomePage(
+                                        etkinlikAdlariList: FirebaseExample
+                                            .PostEtkinlikAdlari, //Ana sayfaya geri dönme
+                                        etkinlikProfilResim:
+                                            FirebaseExample.PostProfileImgs,
+                                        etkinlikProfilIsimList:
+                                            FirebaseExample.PostIsimler,
+                                        etkinlikResimList: FirebaseExample.PostImgs,
+                                        etkinlikTarihList: FirebaseExample.PostTarihler,
+                                        etkinlikKonumList: FirebaseExample.PostKonum),
+                                  ),
+                                  (route) => false,
+                                );
+                              },
                             ),
-                          );
-                        }),
+                          ],
+                        ),
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: IconButton(
+                            onPressed: (() {
+                              Navigator.pushAndRemoveUntil(
+                                context,
+                                MaterialPageRoute(
+                                  // Profil >>> Profil Editleme
+                                  builder: (BuildContext context) => Profile_Edit(),
+                                ),
+                                (route) => false,
+                              );
+                              ; //Burası profil bilgileri değiştirme kısmı
+                            }),
+                            icon: Icon(Icons.edit, size: 25, color: Colors.blue),
+                          ),
+                        ),
+                        Photo_Edit(user: user), //!API!
+
+                        SizedBox(height: 15),
+                        Text(
+                          "${user.name}",
+                          style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+                        ), // İsim
+
+                        //Diğer Bilgiler
+                        Text("${user.location}"),
+                        Column(children: [
+                          Text(
+                            "Meslek: ${user.job}",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Text("Yaş: ${user.age}",
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                        ]),
+
+                        Column(
+                          children: [
+                            Container(
+                              height: MediaQuery.of(context).size.height / 2,
+                              width: MediaQuery.of(context).size.width,
+                              child: ListView.separated(
+                                  shrinkWrap: true,
+                                  scrollDirection: Axis.vertical,
+                                  itemCount: user.Postnumber,
+                                  separatorBuilder: (context, _) => SizedBox(height: 5),
+                                  itemBuilder: (BuildContext context, int index) {
+                                    return Container(
+                                      child: Column(
+                                        children: [
+                                          Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  "Etkinlik Saati:20.00",
+                                                ),
+                                                Text("Etkinlik Yeri:İstanbul"),
+                                                Text("Tarih: 21.06.2022")
+                                              ]),
+                                          SizedBox(
+                                              child: Image.network(
+                                            'https://previews.123rf.com/images/captainvector/captainvector1703/captainvector170301312/73848230-logo-universit%C3%A9.jpg',
+                                            height: 330,
+                                          )),
+                                          Divider(
+                                            thickness: 5,
+                                            indent: 20,
+                                            endIndent: 20,
+                                            color: Colors.black,
+                                          )
+                                        ],
+                                      ),
+                                    );
+                                  }),
+                            ),
+                          ],
+                        )
+                      ]),
+                    ],
                   ),
-                ],
-              )
-            ]),
           ),
-        ));
+              );
   }
 
   void Post_Arttir() {
